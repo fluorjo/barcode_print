b854f8a4712d174fc1d81ed4ac6011fe65212eb5
fluorjo <82512693+fluorjo@users.noreply.github.com>
2021-08-27 16:40:57 +0900

    Update 바코드입력라인.py

--- 바코드입력라인.py
+++ 바코드입력라인.py
@@ -10,19 +10,26 @@ import barcode
 from barcode.writer import ImageWriter
 from barcode import EAN13
 from PIL import Image
+#바코드를 EAN13형식으로 받음.
 hr=barcode.get_barcode_class('ean13')
 
 import tkinter
 from math import *
+from tkinter import *
+from tkinter import ttk
 
 window=tkinter.Tk()
+#바코드 숫자를 입력받는 부분
+a=StringVar()
+
 window.title("바코드 입력창")
-window.geometry("200x50")
+window.geometry("300x300")
 window.resizable(False, False)
 
 def bar(event):
-    number=input()
-    j=EAN13(number,writer=ImageWriter())
+    #이 j 값이 항상 숫자여야 한다면서, 저 'a'값을 받는 게 안 되는데... 
+    #어떻게 해야 UI 내에서 받도록 할 수 있는지 모르겠습니다...! 
+    j=EAN13(a.get("1.0","end"),writer=ImageWriter())
     j.save("barcode")   
     image = Image.open('C:/Users/fluor/barcode.png')
     resize_image = image.resize((400,400))
